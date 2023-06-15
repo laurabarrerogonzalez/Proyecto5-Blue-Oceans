a5e74ed754935a7fcda43b58ba473dbf61b75fae
laurabarrerogonzalez <laura.bgpr94@gmail.com>
2023-06-15 20:35:40 +0200

    Add-start-proyect

---
+++ index.js
@@ -0,0 +1,27 @@
+function loadContent(page) {
+    var xhttp = new XMLHttpRequest();
+    xhttp.onreadystatechange = function() {
+      if (this.readyState === 4 && this.status === 200) {
+        document.getElementById('content').innerHTML = this.responseText;
+      }
+    };
+    xhttp.open('GET', page + '.html', true);
+    xhttp.send();
+  }
+  
+  // Manejo de eventos de clic en los enlaces de navegación
+  
+  document.addEventListener('DOMContentLoaded', function() {
+    var navLinks = document.querySelectorAll('nav ul li a');
+    for (var i = 0; i < navLinks.length; i++) {
+      navLinks[i].addEventListener('click', function(e) {
+        e.preventDefault();
+        var page = this.getAttribute('href').substring(1);
+        loadContent(page);
+      });
+    }
+  });
+  
+  // Carga la página inicial
+  
+  loadContent('home'+ 'about');
